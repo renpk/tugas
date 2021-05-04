d6b97b45e51938c7a207ef82b6fcbebe37a2abd6
renpk <rendykolot21@gmail.com>
2021-05-05 00:30:37 +0700

    Tugas Pertama dan Tugas Kedua :v

--- tugas-while-do.php
+++ tugas-while-do.php
@@ -1,16 +1,44 @@
 <?php 
 
-	$a = 200;
 
+/* 	200 adalah bilangan genap
+ 	199 adalah bilangan ganjil
+ 	dst
+ */
+
+	$a = 200;
+	
 	while($a >= 80) {
-		echo $a . "<br>";
+			
+		if ($a % 2 == 0 ) {
+			echo " ini bilangan Genap = ";
+		}else{
+			echo " ini bilangan Ganjil = ";
+		  }
+
+		echo $a;
+		echo "<br>";
 		$a--;
 	}
 
-	do{
-   		echo  $a . "<br>";
-   	  	$a--;
 
-	}while ($a >= 80)
+	echo "<br>";
+	echo "<br>";
+	echo "<br>";
+
+	
+	$b = 200;
+
+	do{
+		if($a &2 ==0){
+			echo "ini bilangan Genap = ";
+		}else{
+			echo "ini bilangan Ganjil = ";
+		}
+
+   		echo $b . "<br>";
+   		$b--;
+
+	}while ($b >= 80)
 
  ?>
--- tugas.php
+++ tugas.php
@@ -25,160 +25,92 @@
 	Capicorn 	(21 Des - 19 Jan)
 */
 
-	$tanggal 	= 20;
+	$tanggal 	= 19;
 	$bulan  	= 1;
-	$tanggal	= 18;
-	$bulan		= 2 ;
+	
 
 	if ($bulan === 1 && $tanggal >=20 || $bulan === 2 && $tanggal <=18 ) {
 		echo "Benar itu zodiak Aquarius";
-	}else{
-		echo "Salah";
 	}
 
 echo "<br>";
 
-	$tanggal 	= 19;
-	$bulan  	= 2;
-	$tanggal	= 20;
-	$bulan		= 3 ;
-
+	
 	if ($bulan === 2 && $tanggal >=19 || $bulan === 3 && $tanggal <=20 ) {
 		echo "Benar itu zodiak Pisces";
-	}else{
-		echo "Salah";
 	}
 
 echo "<br>";
 
-	$tanggal 	= 21;
-	$bulan  	= 3;
-	$tanggal	= 20;
-	$bulan		= 4 ;
-
+	
 	if ($bulan === 3 && $tanggal >=21 || $bulan === 4 && $tanggal <=20 ) {
 		echo "Benar itu zodiak Aries";
-	}else{
-		echo "Salah";
 	}
 
 echo "<br>";	
 
-	$tanggal 	= 21;
-	$bulan  	= 4;
-	$tanggal	= 20;
-	$bulan		= 5 ;
-
+	
 	if ($bulan === 4 && $tanggal >=21 || $bulan === 5 && $tanggal <=20 ) {
 		echo "Benar itu zodiak Taurus";
-	}else{
-		echo "Salah";
 	}
 
 echo "<br>";
 
-	$tanggal 	= 21;
-	$bulan  	= 5;
-	$tanggal	= 20;
-	$bulan		= 6 ;
+	
 
 	if ($bulan === 5 && $tanggal >=21 || $bulan === 6 && $tanggal <=20 ) {
 		echo "Benar itu zodiak Gemini";
-	}else{
-		echo "Salah";
 	}
 
 echo "<br>";
 
-	$tanggal 	= 21;
-	$bulan  	= 6;
-	$tanggal	= 20;
-	$bulan		= 7 ;
+	
 
 	if ($bulan === 6 && $tanggal >=21 || $bulan === 7 && $tanggal <=20 ) {
 		echo "Benar itu zodiak Cancer";
-	}else{
-		echo "Salah";
 	}
 
 echo "<br>";
 
-	$tanggal 	= 21;
-	$bulan  	= 7;
-	$tanggal	= 21;
-	$bulan		= 8 ;
+	
 
 	if ($bulan === 7 && $tanggal >=21 || $bulan === 8 && $tanggal <=21 ) {
 		echo "Benar itu zodiak Leo";
-	}else{
-		echo "Salah";
 	}
-
 echo "<br>";	
 
-	$tanggal 	= 22;
-	$bulan  	= 8;
-	$tanggal	= 22;
-	$bulan		= 9 ;
+	
 
 	if ($bulan === 8 && $tanggal >=22 || $bulan === 9 && $tanggal <=22 ) {
 		echo "Benar itu zodiak Virgo";
-	}else{
-		echo "Salah";
 	}
-
 echo "<br>";	
 
-	$tanggal 	= 23;
-	$bulan  	= 9;
-	$tanggal	= 12;
-	$bulan		= 10 ;
+	
 
 	if ($bulan === 9 && $tanggal >=23 || $bulan === 10 && $tanggal <=12 ) {
 		echo "Benar itu zodiak Libra";
-	}else{
-		echo "Salah";
 	}
-
 echo "<br>";	
 
-	$tanggal 	= 23;
-	$bulan  	= 10;
-	$tanggal	= 22;
-	$bulan		= 11 ;
-
+	
 	if ($bulan === 10 && $tanggal >=23 || $bulan === 11 && $tanggal <=22 ) {
 		echo "Benar itu zodiak Scorpio";
-	}else{
-		echo "Salah";
 	}
-
 echo "<br>";	
 
-	$tanggal 	= 23;
-	$bulan  	= 11;
-	$tanggal	= 20;
-	$bulan		= 12 ;
+	
 
 	if ($bulan === 11 && $tanggal >=23 || $bulan === 12 && $tanggal <=20 ) {
 		echo "Benar itu zodiak Sagitarius";
-	}else{
-		echo "Salah";
 	}
-
 echo "<br>";	
 
-	$tanggal 	= 21;
-	$bulan  	= 12;
-	$tanggal	= 19;
-	$bulan		= 1 ;
+	
 
 	if ($bulan === 12 && $tanggal >=21 || $bulan === 1 && $tanggal <=19 ) {
 		echo "Benar itu zodiak Capicorn";
-	}else{
-		echo "Salah";
 	}
-
 	 ?>
 
 </body>
