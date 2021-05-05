9ae8cf3db1ebd3a83e39fdeff356f7e0563632aa
renpk <rendykolot21@gmail.com>
2021-05-05 17:03:39 +0700

    masih proses

---
+++ tugas-for-while-do.php
@@ -0,0 +1,18 @@
+<?php 
+
+	// Moch Ilham Rendiansyah
+
+	for($a = 13; $a >= 0; $a--)
+		{
+			if($a - 8 == 5){
+				echo $a . "  Hasilnya Benar" . "<br>" . "<br>";
+			}else{
+				echo $a . " Hasilnya Tidak Sesuai" . "<br>";
+		}
+		
+	}
+
+	
+
+
+ ?>
