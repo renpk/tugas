06b2b6f3bc4a9ceead3e08d9de830d1b92e45bf0
renpk <rendykolot21@gmail.com>
2021-05-05 18:50:41 +0700

    Tugas selesai

--- tugas-for-while-do.php
+++ tugas-for-while-do.php
@@ -2,17 +2,42 @@
 
 	// Moch Ilham Rendiansyah
 
-	for($a = 13; $a >= 0; $a--)
+	$h = 8;
+
+	for($m = 13; $m >= $h; $m--)
 		{
-			if($a - 8 == 5){
-				echo $a . "  Hasilnya Benar" . "<br>" . "<br>";
+			if( ($m - $h == 5) ){
+				echo "  Hasilnya Benar" . "<br>" . "<br>";
 			}else{
-				echo $a . " Hasilnya Tidak Sesuai" . "<br>";
+				echo " Hasilnya Tidak Sesuai" . "<br>";
 		}
 		
 	}
 
-	
+	// $m = 13;
+	// $i = 9;
 
+	// while ($m >= $i) 
+	// {
+	// 	if ( ($m - $i == 4) ) {
+	// 		echo "Hasilnya sangat Benar". "<br>" . "<br>";
+	// 	}else{
+	// 		echo "Hasilnya Salah" . "<br>";
+	// 	  }
+	// 	$m--;
+	// }
+		
+		// $r = 18;
+		// $h = 8;
+
+		// do {
+		// 	if ( ($r - $h == 10) ) {
+		// 		echo "Hasil ya Benar". "<br>". "<br>" ;
+		// 	}else{
+		// 		echo "Hasilnya kurang tepat". "<br>" ;
+		// 	}
+
+		// 	$r--;
+		// }while ($r >= $h);
 
  ?>
