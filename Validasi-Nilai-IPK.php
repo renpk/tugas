69a07fb730b44bf174d3f46789af6c50add850d0
renpk <rendykolot21@gmail.com>
2021-05-25 11:15:03 +0700

    Masih Ragu

--- Validasi-Nilai-IPK.php
+++ Validasi-Nilai-IPK.php
@@ -29,8 +29,6 @@ echo "<br>";
 		echo "SKS yang bisa di ambil hanya 18 SKS";
 	}else if($IPK < 2.5){
 		echo "SKS yang bisa di ambil hanya 18 SKS";
-	}else{
-		echo "SKS tidak bisa diambil";
 	}
 
 echo "<br>";	
@@ -39,8 +37,6 @@ echo "<br>";
 		echo "SKS yang bisa di ambil hanya 15 SKS";
 	}else if($IPK < 2){
 		echo "SKS yang bisa di ambil hanya 15 SKS";
-	}else{
-		echo "SKS tidak bisa diambil";
 	}
 
 echo "<br>";	
