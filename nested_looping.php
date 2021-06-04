97e3651a290b4ea09d3e30d065367bef2a7f0cb8
renpk <rendykolot21@gmail.com>
2021-06-04 10:29:28 +0700

    Bismillah

--- nested_looping.php
+++ nested_looping.php
@@ -1,9 +1,10 @@
 <?php 
 
 	for ($i = 1; $i <= 20; $i++) { 
-			echo 'Looping 1 : ' . $i . '<pre> ';
+			echo "<pre>";
+			echo 'Looping 1 : ' . $i . '<br> ';
 		for ($a = 2; $a <= 20 ; $a+=2) { 
-			echo ' Tampilkan Bilangan Genap ' . $a . ' <br> ' ;
+			echo ' Contoh Bilangan Genap ' . $a . ' <br> ' ;
 		}
 		echo "<br>";
 	}
